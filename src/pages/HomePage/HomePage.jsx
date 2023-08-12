68db30bed6465d99e7556b9b5d46981148eb1fef
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-08-12 13:24:31 +0300

    Added text to Homepage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,94 @@
 const HomePage = () => {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>Welcome to PhoneBookApp!</h1>
+      <p>
+        Create Your Personalized Contact Book
+        <br />
+        Discover the ultimate tool for managing your contacts effectively. With
+        our user-friendly application, you can easily organize and access all
+        your contacts in one place. Whether it's for personal or professional
+        use, ContactBookApp simplifies contact management.
+      </p>
+      <h3>Key Features:</h3>
+      <ul>
+        <li>
+          <p>
+            <strong>Effortless Contact Management:</strong>Create your own
+            digital contact book effortlessly. No more sifting through scattered
+            information – keep everything organized and accessible.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>User-Friendly Interface:</strong>Our intuitive interface
+            ensures a seamless experience, making it easy to add, edit, and
+            delete contacts.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Quick Search:</strong>Looking for a specific contact? Our
+            search feature allows you to find contacts instantly by name.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Secure and Private:</strong>Your data is your own. We
+            prioritize your privacy and data security, ensuring your information
+            remains confidential.
+          </p>
+        </li>
+      </ul>
+
+      <h3>Getting Started:</h3>
+      <ol>
+        <li>
+          <p>
+            <strong>Sign Up or Log In:</strong>If you're new here, create an
+            account to get started. For returning users, simply log in to access
+            your existing account.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Add Contacts:</strong>Create new contacts by entering their
+            name and phone number. Never lose track of important connections
+            again.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Organize with Ease:</strong>Edit, delete, or update contact
+            details at any time. Our platform gives you the flexibility to
+            manage your contacts as your relationships evolve.
+          </p>
+        </li>
+
+        <li>
+          <p>
+            <strong>Effortless Deletion:</strong>Remove contacts you no longer
+            need with just a click. Say goodbye to outdated information
+            cluttering your contact list.
+          </p>
+        </li>
+        <li>
+          <p>
+            <strong>Stay Connected:</strong>Keep your contacts up to date and
+            maintain seamless communication with everyone important to you.
+          </p>
+        </li>
+      </ol>
+      <p>
+        Are you ready to take control of your contacts? Join ContactBookApp
+        today and experience the convenience of managing your contacts
+        hassle-free. Start building your personalized contact book now!
+      </p>
+      <p>Sign Up Now | Log In</p>
+      <p>Got questions? Check out our FAQ section or reach out to our support team for assistance.</p>
+      <p>Stay connected, stay organized with ContactBookApp!</p>
+    </div>
+  );
 };
 
 export default HomePage;
