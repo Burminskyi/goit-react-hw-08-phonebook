989474bb7ab92ae4bdc08f3245f4f2a72af5bec7
Burminskyi <119361840+Burminskyi@users.noreply.github.com>
2023-08-14 18:03:28 +0300

    Added link to logo

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -6,25 +6,25 @@ import logo from './header-logo.svg';
 import IsAuthorized from 'components/IsAuthorized/IsAuthorized';
 import IsNotAuthorized from 'components/IsNotAuthorized/IsNotAuthorized';
 import { selectAuthentificationStatus } from 'redux/auth/selector';
-import { StyledNavbarBrand } from './NavBar.styled';
+import { StyledLogo, StyledNavbarBrand } from './NavBar.styled';
 
 const NavBar = () => {
   const isAuthentificated = useSelector(selectAuthentificationStatus);
   return (
     <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
       <Container>
-      <StyledNavbarBrand style={{ fontSize: '32px', marginRight: '60px' }}>
-          <img
-            alt=""
-            src={logo}
-            width="48"
-            height="48"
-            className="d-inline-block align-top"
-            style={{ fontSize: '32px'}}
-          />{' '}
-          PhoneBook
-        </StyledNavbarBrand>
-        {/* <StyledNavbarBrand>PhoneBook</StyledNavbarBrand> */}
+        <StyledLogo to="/">
+          <StyledNavbarBrand style={{ fontSize: '32px' }}>
+            <img
+              alt=""
+              src={logo}
+              width="48"
+              height="48"
+              className="d-inline-block align-top"
+            />{' '}
+            PhoneBook
+          </StyledNavbarBrand>
+        </StyledLogo>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
--- src/components/NavBar/NavBar.styled.js
+++ src/components/NavBar/NavBar.styled.js
@@ -1,7 +1,12 @@
 import { NavbarBrand } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import { styled } from 'styled-components';
 
 export const StyledNavbarBrand = styled(NavbarBrand)`
   font-size: 32px;
   margin-right: 60px;
+`;
+
+export const StyledLogo = styled(NavLink)`
+  text-decoration: none;
 `;
